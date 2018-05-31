a574f25acfd32cebee520ccead2289b42c9ab176
Robert Brown <robertlb4@gmail.com>
2018-05-30 21:57:43 -0700

    comment out Appuser.sendEmail

--- server/datasources.development.js
+++ server/datasources.development.js
@@ -3,6 +3,6 @@
 module.exports = {
   mongo: {
     connector: 'mongodb',
-    url: process.env.DATABASE_URL || 'mongodb://wayne-retire:retired@ds014648.mlab.com:14648/ionic-db',
+    url: process.env.DATABASE_URL,
   },
 };
